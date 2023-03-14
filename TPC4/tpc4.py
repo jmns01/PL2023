62b5aa6b04624ea8c747a63470f61f9128dc9348
JoaoManuelNovaisdaSIlva <a91671@alunos.uminho.pt>
2023-03-14 01:57:55 +0000

    tpc4 done

--- TPC4/tpc4.py
+++ TPC4/tpc4.py
@@ -1,3 +1,4 @@
+import json
 import re
 import sys
 
@@ -12,7 +13,9 @@ def parcer(ficheiro):
         for line in file:
             lines = line.strip().split(",")
             text.append(lines)
-    return fields,text
+
+        filtered = [s for s in fields if s != ""]
+    return filtered,text
 
 def tpc4(first_line, lines):
     regex = r"^(.+)\{(\d+)(?:,(\d+))?\}(?:::(sum|media))?$"
@@ -27,45 +30,118 @@ def tpc4(first_line, lines):
             operacao = match.group(4)
 
             if max_val == None:
-                mylista(first_line, lines)
+                mylista(first_line, lines, prefixo, min_val)  # uma lista com tamanho fixo
             elif operacao == None:
-                myintervalo(first_line, lines)
+                myintervalo(first_line, lines, prefixo, min_val, max_val)  # uma lista de intervalos possíveis
             elif operacao == "sum":
-                mysum(first_line, lines)
+                mysum(first_line, lines, prefixo, min_val, max_val)
             elif operacao == "media":
-                mymedia(first_line, lines)
+                mymedia(first_line, lines, prefixo, min_val, max_val)
 
             return [prefixo, min_val, max_val, operacao] # eleminar estes return dps (apenas para teste)
 
     return [first_line, lines]
 
-def mylista(first_line, lines):
-    pass
+def mylista(first_line, lines, prefixo, min_val):
+    static_fields = []
+    for campo in first_line[:-1]:
+        static_fields.append((campo, ""))
+    static_fields.append((prefixo, ""))
 
-def myintervalo(first_line, lines):
-    pass
+    resultado = []
+    i, l = 0, len(lines[0])
+    for list in lines:
+        dic = dict(static_fields)
+        for elemento in list:
+            coluna, valor = static_fields[i]
+            dic[coluna] = elemento
+            if i == l-min_val:
+                dic[coluna] = [int(x) for x in list[-min_val:]]
+                break
+            i += 1
+        i = 0
+        resultado.append(dic)
 
-def mysum(first_line, lines):
-    pass
+    with open("alunos1.json", "w") as f:
+        json.dump(resultado, f)
 
-def mymedia(first_line, lines):
-    pass
+
+def myintervalo(first_line, lines, prefixo, min_val, max_val):
+    static_fields = []
+    for campo in first_line[:-1]:
+        static_fields.append((campo, ""))
+    static_fields.append((prefixo, ""))
+
+    resultado = []
+    i, l = 0, len(lines[0])
+    for list in lines:
+        dic = dict(static_fields)
+        for elemento in list:
+            coluna, valor = static_fields[i]
+            dic[coluna] = elemento
+            if i == l - max_val:
+                dic[coluna] = [int(x) for x in list[i:i+max_val] if x != ""]
+                break
+            i += 1
+        i = 0
+        resultado.append(dic)
+
+    with open("alunos2.json", "w") as f:
+        json.dump(resultado, f)
+
+def mysum(first_line, lines, prefixo, min_val, max_val):
+    static_fields = []
+    for campo in first_line[:-1]:
+        static_fields.append((campo, ""))
+    static_fields.append((prefixo, ""))
+
+    resultado = []
+    i, l = 0, len(lines[0])
+    for list in lines:
+        dic = dict(static_fields)
+        for elemento in list:
+            coluna, valor = static_fields[i]
+            dic[coluna] = elemento
+            if i == l - max_val:
+                valores = [int(x) for x in list[i:i + max_val] if x != ""]
+                dic[coluna] = sum(valores)
+                break
+            i += 1
+        i = 0
+        resultado.append(dic)
+
+    with open("alunos3.json", "w") as f:
+        json.dump(resultado, f)
+
+def mymedia(first_line, lines, prefixo, min_val, max_val):
+    static_fields = []
+    for campo in first_line[:-1]:
+        static_fields.append((campo, ""))
+    static_fields.append((prefixo, ""))
+
+    resultado = []
+    i, l = 0, len(lines[0])
+    for list in lines:
+        dic = dict(static_fields)
+        for elemento in list:
+            coluna, valor = static_fields[i]
+            dic[coluna] = elemento
+            if i == l - max_val:
+                valores = [int(x) for x in list[i:i + max_val] if x != ""]
+                dic[coluna] = sum(valores)/len(valores)
+                break
+            i += 1
+        i = 0
+        resultado.append(dic)
+
+    with open("alunos4.json", "w") as f:
+        json.dump(resultado, f)
 
 
 if __name__ == "__main__":
     ficheiro = sys.argv[1]
 
     fst, lines = parcer(ficheiro)
-    print(fst)
-    print(lines)
+    tpc4(fst, lines)
 
-    if len(tpc4(fst, lines)) > 2:
-        pre, min, max, op = tpc4(fst, lines)
-        print(pre)
-        print(min)
-        print(max)
-        print(op)
-    else:
-        fst1, lines1 = tpc4(fst, lines)
-        print(fst1)
-        print(lines1)
+
